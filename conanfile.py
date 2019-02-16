5e79063e8c723aced93e85306eeef51b9c23317c
seladb <seladb@yahoo.com>
2019-02-15 16:33:59 -0800

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -118,6 +118,8 @@ class PcapplusplusConan(ConanFile):
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.libs.extend(["pthread"])
+        if self.settings.os == "Windows":
+            self.cpp_info.libs.extend(["Iphlpapi"])
 
     def generate_directory_build_props_file(self):
     
